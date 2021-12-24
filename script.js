4f10c671e0b4a027310094758f764272d4a8b7dd
PO144 <74781521+PO144@users.noreply.github.com>
2021-12-24 20:14:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,6 @@ var container = document.getElementsByClassName('container')[0];
 document.addEventListener('click', function( event ) {
   if (container == event.target || container.contains(event.target)) {    
     snow.toggle();
+    console.log("Test");
   }
 });
