5cfe1ffb98dfbeab1a8b8dccbcbf8c4491ab2fc6
PO144 <74781521+PO144@users.noreply.github.com>
2021-12-24 20:25:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-import * as Snow from './snow.js'
-
-var snow = new Snow.default({
-    id: 'snow',
-    theme: 'berry',
-    min_size: 1,
-    max_size: 5
-});
-
-/*var container = document.getElementsByClassName('container')[0];
+var container = document.getElementsByClassName('container')[0];
 document.addEventListener('click', function( event ) {
-  if (container == event.target || container.contains(event.target)) {    
-    snow.toggle();
-    console.log("Test");
+  if (container == event.target || container.contains(event.target)) {
+    var falling = new Blizzard('container');
   }
-});*/
-snow.toggle();
+});
