ae5b42282ddf4297fdb193b45f63fd39960e24c7
PO144 <74781521+PO144@users.noreply.github.com>
2021-12-24 20:30:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+var falling = new Blizzard('container');
+/*
 var container = document.getElementsByClassName('container')[0];
 document.addEventListener('click', function( event ) {
   if (container == event.target || container.contains(event.target)) {
-    var falling = new Blizzard('container');
+    
   }
-});
+});*/
