6148b8f0975dbac85bfd96ce13e5992312ff8adb
PO144 <74781521+PO144@users.noreply.github.com>
2021-12-24 20:33:04 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var falling = new Blizzard('container');
-/*
-var container = document.getElementsByClassName('container')[0];
-document.addEventListener('click', function( event ) {
-  if (container == event.target || container.contains(event.target)) {
-    
-  }
-});*/
